3f8ca708619f0b2464eeb46b6557169d41b2d634
rodolfo.dure90@gmail.com <rodolfo.dure90@gmail.com>
2024-07-09 15:58:23 -0400

    Se añadio images - js - style css

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$("#card1").text("Texto Actualizado")
+$("#card2").text("Texto Actualizado Numero 2")
+$("#card3").text("Texto Actualizado Numero 3")
+$("#card4").text("Texto Actualizado Numero 4")
+$("#card5").text("Texto Actualizado Numero 5")
+$("#card6").text("Texto Actualizado Numero 6")
+$("#card7").text("Texto Actualizado Numero 7")
+$("#card8").text("Texto Actualizado Numero 8")
+//evento clic para expand img
+$('.card-imagen').on('click',function() {
+    $(this).alert('expanded');
+    
+});
+
